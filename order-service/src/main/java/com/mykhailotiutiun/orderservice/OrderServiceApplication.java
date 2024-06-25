5312614970fd1c18bfd2236379889c3bc5123652
Mykhailo Tiutiun <mykhialotiutiun@gmail.com>
2024-06-25 12:13:42 +0200

    Create discovery-service  - Create discovery-service  - Register other services to it

--- order-service/src/main/java/com/mykhailotiutiun/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/mykhailotiutiun/orderservice/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.mykhailotiutiun.orderservice;
 import io.katharsis.spring.boot.v3.KatharsisConfigV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
